55a50f7d7e009dd11bd3e09c54463cafa199d1dd
Mike Fallesen <mfallesen2010@gmail.com>
2020-10-06 22:18:23 -0700

    added images and README information

--- app.js
+++ app.js
@@ -17,97 +17,98 @@ const render = require("./lib/htmlRenderer");
 
 const employeeArr = [];
 
-function askUser() { 
+function askUser() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What the employee's name?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "What the employee's Employee ID?",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What the employee's Email Address?",
-            name: "email"
-        },
-        {
-            type: "list",
-            message: "what is the employee's role?",
-            name: "role",
-            choices: ["Manager", "Engineer", "Intern"]
-        },
-        {
-            type: "input",
-            message: "What is the manager's office number?",
-            name: "officeNumber",
-            when: function(response) {
-                return response.role === "Manager";
-            }
-        },
-        {
-            type: "input",
-            message: "What is the engineers's Github username?",
-            name: "github",
-            when: function(response) {
-                return response.role === "Engineer";
-            }
-        },
-        {
-            type: "input",
-            message: "What is the intern's school?",
-            name: "school",
-            when: function(response) {
-                return response.role === "Intern";
-            }
-        },
-        {
-            type: "confirm",
-            message: "Would you like to add another Employee?",
-            name: "anotheremployee",
-        },
-    ])
-    .then(function (response) {
+        .prompt([
+            {
+                type: "input",
+                message: "What the employee's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What the employee's Employee ID?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What the employee's Email Address?",
+                name: "email"
+            },
+            {
+                type: "list",
+                message: "what is the employee's role?",
+                name: "role",
+                choices: ["Manager", "Engineer", "Intern"]
+            },
+            {
+                type: "input",
+                message: "What is the manager's office number?",
+                name: "officeNumber",
+                when: function (response) {
+                    return response.role === "Manager";
+                }
+            },
+            {
+                type: "input",
+                message: "What is the engineers's Github username?",
+                name: "github",
+                when: function (response) {
+                    return response.role === "Engineer";
+                }
+            },
+            {
+                type: "input",
+                message: "What is the intern's school?",
+                name: "school",
+                when: function (response) {
+                    return response.role === "Intern";
+                }
+            },
+            {
+                type: "confirm",
+                message: "Would you like to add another Employee?",
+                name: "anotheremployee",
+            },
+        ])
+        .then(function (response) {
 
-        console.table(response);
-        switch (response.role) {
-            case 'Manager':
-                
-                const manager = new Manager (response.name, response.id, response.email, response.officeNumber);
-                employeeArr.push(manager);
-                break;
-            case 'Engineer':
-                
-                const engineer = new Engineer (response.name, response.id, response.email, response.github );
-                employeeArr.push(engineer);
-                break;
-            case 'Intern':
-                
-                const intern = new Intern (response.name, response.id, response.email, response.school);
-                employeeArr.push(intern);
-                break;
-            default :
-                console.log(`Sorry something went wrong`);
-        }
-        
-       if (response.anotheremployee === true ) {
-           askUser();
-       } else {
-           console.log(employeeArr);
-           const html = render(employeeArr);
-           fs.writeFile(outputPath, html, (err) => {
-               if (err) throw err;
-               console.log("Finished!");
-           })
-           console.log(html);
-       }
+            console.table(response);
+            switch (response.role) {
+                case 'Manager':
+
+                    const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+                    employeeArr.push(manager);
+                    break;
+
+                case 'Engineer':
+
+                    const engineer = new Engineer(response.name, response.id, response.email, response.github);
+                    employeeArr.push(engineer);
+                    break;
+                case 'Intern':
+
+                    const intern = new Intern(response.name, response.id, response.email, response.school);
+                    employeeArr.push(intern);
+                    break;
+                default:
+                    console.log(`Sorry something went wrong`);
+            }
+
+            if (response.anotheremployee === true) {
+                askUser();
+            } else {
+                console.log(employeeArr);
+                const html = render(employeeArr);
+                fs.writeFile(outputPath, html, (err) => {
+                    if (err) throw err;
+                    console.log("Finished!");
+                })
+                console.log(html);
+            }
 
 
-    });
+        });
 }
 
 askUser();
